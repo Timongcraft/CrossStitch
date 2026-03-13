9a42998ad3f2e5395d87d056118e3fc57c317013
Timon Seidel <timong.seidel@gmail.com>
2026-03-13 16:35:12 +0100

    feat: update to 26.1-pre-2

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ tasks.named<ProcessResources>("processResources") {
     val props = mapOf(
         "version" to libs.versions.project.get(),
         "java" to libs.versions.java.get(),
-        "minecraft" to libs.versions.minecraft.get(), //todo: re-add placeholder for release version -> in fabric.mod.json
+        "minecraft" to libs.versions.minecraft.get(),
         "fabric_loader" to libs.versions.fabric.loader.get()
     )
 
